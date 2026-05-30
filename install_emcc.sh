6826cb89dcb2f6f204b3bd69c60ede066635b8ac
Paul Taylor <60293767+Y0ursTruly@users.noreply.github.com>
2026-05-30 09:00:00 -0400

    Update install_emcc.sh

--- install_emcc.sh
+++ install_emcc.sh
@@ -3,3 +3,7 @@ cd emsdk
 ./emsdk install latest
 ./emsdk activate latest
 . ./emsdk_env.sh
+
+#building with emcc is as follows
+#  echo -e "CC = emcc\nCXX = em++\nAR=emar\nRANLIB=emranlib\nCXXFLAGS=-msimd128" >> Makefile
+#  emmake make -j $(nproc) build-target-if-any
